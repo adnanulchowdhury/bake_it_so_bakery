50a85cc6d87bd6b427b7f8cf00970b1416ee77f3
Md adnanul Chowdhury <adnanul1993@gmail.com>
2018-04-27 08:07:09 -0400

    added some materials before presentations

--- bake_it_so.rb
+++ bake_it_so.rb
@@ -27,7 +27,36 @@ post "/" do
     from = Email.new(email: 'bakeitso@gmail.com')
     to = Email.new(email: params[:email])
     subject = "Bake It So Newsletter"
-    content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+    content = Content.new(type: 'text/html', value: 
+    '<h2>Bake It So!</h2>
+     <h6>Cakes</h6>
+        <ul>
+            <li>Sci-Fi themed cake.</li>
+            <li>Strawberry Cream cake.</li>
+            <li>Ultimate Chocolate cake.</li>
+            <li>Red Velvet cake.</li>
+            <li>Mango Blast cake.</li>
+            <li>Blueberry layered cake.</li>
+        </ul>
+     <h6>Cake Pops</h6>
+        <ul>
+            <li>Strawberry Cake Pop.</li>
+            <li>Red Velvet Cake Pop.</li>
+            <li>Blueberry Cake Pop.</li>
+            <li>Rainbow Cake Pop.</li>
+            <li>Golden Snitch Pop.</li>
+            <li>Watermelon Pop.</li>
+        </ul>
+     <h6>Cookies</h6>
+        <ul>
+            <li>Peanut Butter.</li>
+            <li>Oatmeal Raisin.</li>
+            <li>Cheescake Cookie.</li>
+            <li>Chocolate Chip.</li>
+            <li>White Chocolate Chip.</li>
+            <li>Red Velvet.</li>
+        </ul>
+    ')
     mail = Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
